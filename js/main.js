1398afac5710b85b2885127ee7522bd7a32e1f75
aleix <al3ixgh@gmail.com>
2021-06-03 17:28:03 +0200

    css enlazado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-addEventListener("load",inicializarEventos,false);
+addEventListener('load',inicializarEventos,false);
 
 function inicializarEventos(){
     getAll();
@@ -6,6 +6,20 @@ function inicializarEventos(){
 
 function getAll(){
     $.ajax({
-        url:"https://rickandmortyapi.com"
+        url:"https://rickandmortyapi.com/api/character/326",
+        method: 'GET',
+        dataType: 'Json',
+        headers: {
+            'Accept':'application/json'
+        },
+        contentType: 'application/x-www-form-urlencoded',
+
+        success: function(data){
+            console.log(JSON.stringify(data));
+            document.getElementById("img1").src.innerHTML=data.image;
+        },
+        error: function (error){
+            console.log(error);
+        }
     })
 }
