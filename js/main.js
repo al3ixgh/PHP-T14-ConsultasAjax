a48747735301ff3c6303c84ece27dc79897be634
aleix <al3ixgh@gmail.com>
2021-06-03 19:11:33 +0200

    js acabado

--- js/main.js
+++ js/main.js
@@ -13,13 +13,100 @@ function getAll(){
             'Accept':'application/json'
         },
         contentType: 'application/x-www-form-urlencoded',
-
         success: function(data){
             console.log(JSON.stringify(data));
-            console.log(data.image);
             document.getElementById("img1").src=data.image;
-            // document.getElementById("img1").attr('src',data.image);
-            document.getElementById("title1").innerHTML=data.name;
+            $("#title1").html(data.name);
+
+        },
+        error: function (error){
+            console.log(error);
+        }
+    })
+    $.ajax({
+        url:"https://rickandmortyapi.com/api/character/316",
+        method: 'GET',
+        dataType: 'Json',
+        headers: {
+            'Accept':'application/json'
+        },
+        contentType: 'application/x-www-form-urlencoded',
+        success: function(data){
+            console.log(JSON.stringify(data));
+            document.getElementById("img2").src=data.image;
+            $("#title2").html(data.name);
+
+        },
+        error: function (error){
+            console.log(error);
+        }
+    })
+    $.ajax({
+        url:"https://rickandmortyapi.com/api/character/306",
+        method: 'GET',
+        dataType: 'Json',
+        headers: {
+            'Accept':'application/json'
+        },
+        contentType: 'application/x-www-form-urlencoded',
+        success: function(data){
+            console.log(JSON.stringify(data));
+            document.getElementById("img3").src=data.image;
+            $("#title3").html(data.name);
+
+        },
+        error: function (error){
+            console.log(error);
+        }
+    })
+    $.ajax({
+        url:"https://rickandmortyapi.com/api/character/206",
+        method: 'GET',
+        dataType: 'Json',
+        headers: {
+            'Accept':'application/json'
+        },
+        contentType: 'application/x-www-form-urlencoded',
+        success: function(data){
+            console.log(data);
+            document.getElementById("img4").src=data.image;
+            $("#title4").html(data.name);
+
+        },
+        error: function (error){
+            console.log(error);
+        }
+    })
+    $.ajax({
+        url:"https://rickandmortyapi.com/api/character/256",
+        method: 'GET',
+        dataType: 'Json',
+        headers: {
+            'Accept':'application/json'
+        },
+        contentType: 'application/x-www-form-urlencoded',
+        success: function(data){
+            console.log(data);
+            document.getElementById("img5").src=data.image;
+            $("#title5").html(data.name);
+
+        },
+        error: function (error){
+            console.log(error);
+        }
+    })
+    $.ajax({
+        url:"https://rickandmortyapi.com/api/character/276",
+        method: 'GET',
+        dataType: 'Json',
+        headers: {
+            'Accept':'application/json'
+        },
+        contentType: 'application/x-www-form-urlencoded',
+        success: function(data){
+            console.log(data);
+            document.getElementById("img6").src=data.image;
+            $("#title6").html(data.name);
 
         },
         error: function (error){
